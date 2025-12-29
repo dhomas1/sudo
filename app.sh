8b87c5cb64faa12b86382add478fb522ffb01829
dhomas1 <dom@cotugno.co>
2025-12-29 14:33:53 -0500

    Update app.sh
    Setting tmpfilesdir

--- app.sh
+++ app.sh
@@ -4,10 +4,11 @@ local VERSION="1.9.17p2"
 local FOLDER="sudo-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="https://www.sudo.ws/sudo/dist/${FILE}"
+local tmpfilesdir="${DEST}/etc/tmpfiles.d"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --host="${HOST}" --prefix="${DEST}" --sysconfdir="${DEST}/etc" --mandir="${DEST}/man" --with-vardir="/tmp/DroboApps/${NAME}/var" --with-iologdir="${DEST}/var/log/sudo-io" --with-rundir="/tmp/DroboApps/${NAME}/run" --with-editor="/bin/vi" --without-systemd --with-tmpfilesdir="${DEST}/etc/tmpfiles.d"
+./configure --host="${HOST}" --prefix="${DEST}" --sysconfdir="${DEST}/etc" --mandir="${DEST}/man" --with-vardir="/tmp/DroboApps/${NAME}/var" --with-iologdir="${DEST}/var/log/sudo-io" --with-rundir="/tmp/DroboApps/${NAME}/run" --with-editor="/bin/vi" --without-systemd
 make
 make install install_uid=$(id -u) install_gid=$(id -g)
 mv "${DEST}/etc/sudoers" "${DEST}/etc/sudoers.orig"
