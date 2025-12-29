169e5fc42ced910b9ac15d1d0713dc308245895d
dhomas1 <dom@cotugno.co>
2025-12-29 14:24:12 -0500

    Update app.sh
    Added missing double quote

--- app.sh
+++ app.sh
@@ -7,7 +7,7 @@ local URL="https://www.sudo.ws/sudo/dist/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --host="${HOST}" --prefix="${DEST}" --sysconfdir="${DEST}/etc" --mandir="${DEST}/man" --with-vardir="/tmp/DroboApps/${NAME}/var" --with-iologdir="${DEST}/var/log/sudo-io" --with-rundir="/tmp/DroboApps/${NAME}/run" --with-editor="/bin/vi --without-systemd --disable-tmpfiles
+./configure --host="${HOST}" --prefix="${DEST}" --sysconfdir="${DEST}/etc" --mandir="${DEST}/man" --with-vardir="/tmp/DroboApps/${NAME}/var" --with-iologdir="${DEST}/var/log/sudo-io" --with-rundir="/tmp/DroboApps/${NAME}/run" --with-editor="/bin/vi" --without-systemd --disable-tmpfiles
 make
 make install install_uid=$(id -u) install_gid=$(id -g)
 mv "${DEST}/etc/sudoers" "${DEST}/etc/sudoers.orig"
